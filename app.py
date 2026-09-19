5af3635c69fe1bbe1aac1613cc5144a309548f62
Fushi <youssefameronly@gmail.com>
2026-09-20 02:29:22 +0300

    Prepare project for public deployment

--- app.py
+++ app.py
@@ -351,4 +351,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print(f"\n[+] SentinelFlow running at http://localhost:{port}")
     print("    Press Ctrl+C to stop.\n")
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
